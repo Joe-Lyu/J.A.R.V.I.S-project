c7ae6bb46775b9de816e505bbf88ca659f5b695b
Zhaochen Lyu <68528026+ZhaochenLyu@users.noreply.github.com>
2021-07-28 12:00:36 +0800

    Added model for training and prediction

---
+++ intent.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 27 23:54:11 2021
+
+@author: DELL
+"""
+import os
+import numpy as np
+import pandas as pd
+from  silence_tensorflow import silence_tensorflow
+silence_tensorflow()
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer
+import nltk
+import re
+from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, LSTM, Bidirectional, Embedding, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
+
+proxy = "http://127.0.0.1:15732"
+os.environ['http_proxy'] = proxy 
+os.environ['HTTP_PROXY'] = proxy
+os.environ['https_proxy'] = proxy
+os.environ['HTTPS_PROXY'] = proxy
+def load_dataset(filename):
+    df = pd.read_csv(filename, encoding = "latin1", names = ["Sentence", "Intent"])
+    intent = df["Intent"]
+    unique_intent = list(set(intent))
+    sentences = list(df["Sentence"])
+    
+    return (intent, unique_intent, sentences)
+
+intent, unique_intent, sentences = load_dataset(r"D:\Everything\科研\Python\JARVIS project\Dataset-train.csv")
+stemmer = LancasterStemmer()
+def cleaning(sentences):
+    words = []
+    for s in sentences:
+        clean = re.sub(r'[^ a-z A-Z 0-9]', " ", s)
+        w = word_tokenize(clean)
+        #stemming
+        words.append([i.lower() for i in w])
+  
+    return words 
+cleaned_words = cleaning(sentences)
+
+
+def create_tokenizer(words, filters = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'):
+    token = Tokenizer(filters = filters)
+    token.fit_on_texts(words)
+    return token
+
+
+def max_length(words):
+    return(len(max(words, key = len)))
+  
+word_tokenizer = create_tokenizer(cleaned_words)
+vocab_size = len(word_tokenizer.word_index) + 1
+max_length = max_length(cleaned_words)
+
+def encoding_doc(token, words):
+  return(token.texts_to_sequences(words))
+
+encoded_doc = encoding_doc(word_tokenizer, cleaned_words)
+
+def padding_doc(encoded_doc, max_length):
+  return(pad_sequences(encoded_doc, maxlen = max_length, padding = "post"))
+
+padded_doc = padding_doc(encoded_doc, max_length)
+
+output_tokenizer = create_tokenizer(unique_intent, filters = '!"#$%&()*+,-/:;<=>?@[\]^`{|}~')
+
+encoded_output = encoding_doc(output_tokenizer, intent)
+
+x=[]
+for i in encoded_output:
+    x.append(i[0])
+encoded_output=x
+
+encoded_output = (np.array(encoded_output).reshape(len(encoded_output), 1))
+
+def one_hot(encode):
+    o = OneHotEncoder(sparse = False)
+    return(o.fit_transform(encode))
+
+output_one_hot = one_hot(encoded_output)
+
+train_X, val_X, train_Y, val_Y = train_test_split(padded_doc, output_one_hot, shuffle = True, test_size = 0.2)
+
+def create_model(vocab_size, max_length):
+    model = Sequential()
+    model.add(Embedding(vocab_size, 128, input_length = max_length, trainable = False))
+    model.add(Bidirectional(LSTM(128)))
+    #model.add(LSTM(128))
+    model.add(Dense(32, activation = "relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(32, activation = "relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(150, activation = "softmax"))
+
+    return model
+
+model = create_model(vocab_size, max_length)
+import tensorflow
+opt=tensorflow.keras.optimizers.Adam(learning_rate=9e-4)
+model.compile(loss = "categorical_crossentropy", optimizer = opt, metrics = ["accuracy"])
+
+filename = 'intent.h5'
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+model = load_model("intent.h5")
+def predictions(text):
+  clean = re.sub(r'[^ a-z A-Z 0-9]', " ", text)
+  test_word = word_tokenize(clean)
+  test_word = [w.lower() for w in test_word]
+  test_ls = word_tokenizer.texts_to_sequences(test_word)
+  #Check for unknown words
+  if [] in test_ls:
+    test_ls = list(filter(None, test_ls))
+    
+  test_ls = np.array(test_ls).reshape(1, len(test_ls))
+ 
+  x = padding_doc(test_ls, max_length)
+  
+  pred = model.predict(x)
+  
+  
+  return pred
+
+def get_final_output(pred, classes):
+  predictions = pred[0]
+ 
+  classes = np.array(classes)
+  ids = np.argsort(-predictions)
+  classes = classes[ids]
+  predictions = -np.sort(-predictions)
+ 
+  for i in range(pred.shape[1]):
+    print("%s has confidence = %s" % (classes[i], (predictions[i])))
+
+
+text = "how to say Jarvis in french"
+pred = predictions(text)
+get_final_output(pred, unique_intent)
+
+
+
+
+
