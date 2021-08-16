2b64d0319f131c68b6994d30964e7428aa85b3fd
Joe <joelzc2023@qq.com>
2021-08-16 16:26:47 +0800

    updated animation programs to add lifelike features
    random play/rewind frames of the gif

--- art/animation.py
+++ art/animation.py
@@ -21,9 +21,22 @@ sequence = [ImageTk.PhotoImage(img) for img in ImageSequence.Iterator(Image.open
 
 interframe_duration = Image.open(gif_filename).info['duration']     # get how long to delay between frames
 
+frame=random.choice(sequence)
 while True:
-    for frame in sequence:
+    direction=random.choice([-1,1,1])
+    length=random.randint(1,45)
+    ind=sequence.index(frame)
+    if ind<=length and direction==-1:
+        direction=1
+    elif ind>length and direction==1:
+        direction=-1
+    newseq=[]
+    for i in range(1,length+1):
+        newseq.append(sequence[ind+i*direction])
+    for i in newseq:
         event, values = window.read(timeout=interframe_duration)
         if event == sg.WIN_CLOSED:
             sys.exit()
-        window['-IMAGE-'].update(data=frame)
+        window['-IMAGE-'].update(data=i)
+    frame=newseq[-1]
+    time.sleep(interframe_duration/1000)
